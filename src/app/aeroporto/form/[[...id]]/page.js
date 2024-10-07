aed753ae269cae5d678fedbaa3d03c905f1aa867
lucasrodriguesx <gustavodrdr19@gmail.com>
2024-10-07 19:24:06 -0300

    Voe-mais

--- src/app/aeroporto/form/[[...id]]/page.js
+++ src/app/aeroporto/form/[[...id]]/page.js
@@ -1,4 +1,4 @@
-"use client";
+'use client'
 
 import Pagina from "@/app/components/Pagina";
 import { Formik } from "formik";
@@ -7,23 +7,31 @@ import { useRouter } from "next/navigation";
 import { Button, Form } from "react-bootstrap";
 import { FaCheck } from "react-icons/fa";
 import { MdOutlineArrowBack } from "react-icons/md";
+import { v4 } from "uuid";
 
-export default function Page() {
+export default function Page({ params }) {
+    
   const route = useRouter();
 
+  const aeroportos = JSON.parse(localStorage.getItem("aeroportos")) || [];
+  const dados = aeroportos.find((item) => item.id == params.id);
+  const aeroporto = dados || { nome: "", sigla: "", cidade: "", uf: "", pais: "" };
+
   function salvar(dados) {
-    const aeroportos = JSON.parse(localStorage.getItem("aeroportos")) || [];
-    aeroportos.push(dados);
+    if (aeroporto.id) {
+      Object.assign(aeroporto, dados);
+    } else {
+      dados.id = v4();
+      aeroportos.push(dados);
+    }
+
     localStorage.setItem("aeroportos", JSON.stringify(aeroportos));
     return route.push("/aeroporto");
   }
 
   return (
     <Pagina titulo="Aeroporto">
-      <Formik
-        initialValues={{ nome: "", sigla: "", cidade: "", pais: "" }}
-        onSubmit={(values) => salvar(values)}
-      >
+      <Formik initialValues={aeroporto} onSubmit={(values) => salvar(values)}>
         {({ values, handleChange, handleSubmit }) => (
           <Form>
             <Form.Group className="mb-3" controlId="nome">
@@ -35,6 +43,7 @@ export default function Page() {
                 onChange={handleChange("nome")}
               />
             </Form.Group>
+
             <Form.Group className="mb-3" controlId="sigla">
               <Form.Label>Sigla</Form.Label>
               <Form.Control
@@ -44,6 +53,7 @@ export default function Page() {
                 onChange={handleChange("sigla")}
               />
             </Form.Group>
+
             <Form.Group className="mb-3" controlId="cidade">
               <Form.Label>Cidade</Form.Label>
               <Form.Control
@@ -53,6 +63,17 @@ export default function Page() {
                 onChange={handleChange("cidade")}
               />
             </Form.Group>
+
+            <Form.Group className="mb-3" controlId="uf">
+              <Form.Label>UF</Form.Label>
+              <Form.Control
+                type="text"
+                name="uf"
+                value={values.uf}
+                onChange={handleChange("uf")}
+              />
+            </Form.Group>
+
             <Form.Group className="mb-3" controlId="pais">
               <Form.Label>País</Form.Label>
               <Form.Control
@@ -62,6 +83,7 @@ export default function Page() {
                 onChange={handleChange("pais")}
               />
             </Form.Group>
+
             <div className="text-center">
               <Button onClick={handleSubmit} variant="success">
                 <FaCheck /> Salvar
